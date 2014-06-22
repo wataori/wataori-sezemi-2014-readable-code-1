2136de4ac2eab6852183ba11dab3765b943ce6a9
wataori <ropifuku@gmail.com>
2014-06-22 12:02:16 +0900

    spec6: IDを指定したレシピのみ出力する

--- recipe.rb
+++ recipe.rb
@@ -1,11 +1,16 @@
 # coding: utf-8
 
-def read_output_recipes(file)
+def read_output_recipes(file, id)
   f = open(file)
-  f.each { |line| 
-    puts "#{f.lineno}: #{line}"
-  }
+  if id
+    id = id.to_i
+    puts "#{id}: #{f.readlines[id - 1]}"
+  else
+    f.each { |line| 
+      puts "#{f.lineno}: #{line}"
+    }
+  end
   f.close
 end
 
-read_output_recipes(ARGV[0].to_s)
+read_output_recipes(ARGV[0].to_s, ARGV[1])
